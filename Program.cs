6342248a3983242a206def7f3640044539f83573
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 18:20:48 +0530

    power_2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public void Calulation()
+    {
+        int baseNumber = 2;
+        int powerNumber;
+        int i = 1;
+        Console.Write("Enter power number :");
+        powerNumber = int.Parse(Console.ReadLine());
+        while (i <= powerNumber)
+        {
+            var returnNumber = Math.Pow(baseNumber, i);
+            Console.Write("{0}^{1}={2}\n", baseNumber, i, returnNumber);
+            i++;
+        }
+    }
+}
+class Result
+{
+    public static void Main(string[]args)
+    {
+        Program pro=new Program();
+        pro.Calulation();
+    }
+}
+
